3072efe75979b0fe7aebd0f434710174866289d8
赵明威 <563652043@qq.com>
2016-06-26 12:37:41 +0800

    update comments
    改正注释

--- src/arithApi/Operation.java
+++ src/arithApi/Operation.java
@@ -4,6 +4,6 @@ package arithApi;
  * Created by zhaomingwei on 16/6/19.
  */
 public interface Operation {
-    //返回一组数的最小值
+    //对两个参数进行操作,并且返回整型的参数值
     int operationPredicate(int num1,int num2);
 }
